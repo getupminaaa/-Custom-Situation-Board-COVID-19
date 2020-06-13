9a4713a1126e6fbf809cd299b9fb18eb9c366912
alzseven <alskdjfhg7455@naver.com>
2020-06-13 22:01:00 +0900

    status saving +@

--- costum_app/lib/main.dart
+++ costum_app/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:costumapp/setting.dart';
 
 void main() => runApp(MyApp());
 
+final _fm = FunctionManager();
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -24,8 +28,8 @@ class HomePage extends StatelessWidget {
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("lib/img/background.jpg"), fit: BoxFit.cover),
+            // image: DecorationImage(
+            //     image: AssetImage("lib/img/background.jpg"), fit: BoxFit.cover),
           ),
           child: ListViewLayout(),
         ),
@@ -81,6 +85,7 @@ Widget maskMap() {
   );
 }
 
+//TODO:Fix
 _ListViewLayoutState mListViewState;
 
 class ListViewLayout extends StatefulWidget {
@@ -90,248 +95,57 @@ class ListViewLayout extends StatefulWidget {
 }
 
 class _ListViewLayoutState extends State<ListViewLayout> {
-  List<CustomFunction> Listss = [];
+  //List<CustomFunction> Listss = [];
 
   @override
   Widget build(BuildContext context) {
-    return ReorderableListView(
-      onReorder: (int oldindex, int newindex) {
-        setState(() {
-          var tempfuncs = Listss[oldindex];
-          Listss.add(tempfuncs);
-        });
-      },
-      children: List.generate(Listss.length, (index){return emptyCard(Listss[index]);}),
-    );
-  }
 
-  void mAddFunc(CustomFunction selectedTiles) {
-    setState(() {
-      Listss.add(selectedTiles);
-    });
-  }
-}
-
-class Performance extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Setting"),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("lib/img/background.jpg"), fit: BoxFit.cover),
-          ),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: 400,
-                height: 270,
-                padding: const EdgeInsets.all(6),
-                child: Card(
-                  color: Color(0x59474646),
-                  child: UsableListView(),
-                ),
-              ),
-              Container(
-                width: 400,
-                height: 270,
-                padding: const EdgeInsets.all(6),
-                child: Card(
-                  color: Color(0x59474646),
-                  child: UnusableListView(),
-                ),
-              ),
-              Container(
-                width: 70,
-                height: 30,
-                child: RaisedButton(
-                  padding: const EdgeInsets.all(5),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Text('Go'),
-                ),
-              )
-            ],
-          ),
-        ));
-  }
-}
-
-//unsuableList와 usableList가 필요 unsuable에서 usable로 해당 데이터가 삽입되어야함
-// 삽입될때 인덱스는 어떻게? for문으로 위치를 찾아줘야하나?
-// 삭제또한 마찬가지
-// usable은 reorderable이어야함
-// 이 모든게 성공하면 globalkey로 앞 화면에 적용하고
-// 앞화면의 데이터를 주거나 연결해줘야할 듯
-class CustomFunction {
-  String funcName;
-
-  CustomFunction(String _funcName) {
-    funcName = _funcName;
-  }
-}
-
-_UnusableListViewState unListViewState;
-_UsableListViewState uListViewState;
-
-String unusableItem = "집갈래";
-
-//listview builder로 생성 list.length만큼
-class UnusableListView extends StatefulWidget {
-  @override
-  _UnusableListViewState createState() =>
-      unListViewState = new _UnusableListViewState();
-}
-
-class _UnusableListViewState extends State<UnusableListView> {
-  List<CustomFunction> unusableListTiles = [
-    CustomFunction("MaskMap"),
-    CustomFunction("ReliefHospitals"),
-    CustomFunction("SidoInfos"),
-  ];
-
-  void AddFunc(CustomFunction funcs) {
-    setState(() {
-      unusableListTiles.add(funcs);
-    });
-  }
-
-  void RemoveFunc(CustomFunction funcs) {
-    setState(() {
-      unusableListTiles.remove(funcs);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    //return emptyCard(Listss[index]);
     return ListView.builder(
-      itemCount: unusableListTiles.length,
+      itemCount: _fm.usingFunctions.length,
       itemBuilder: (context, index) {
-        return UnusableListTile(unusableListTiles[index]);
+        return emptyCard(_fm.usingFunctions[index]);
       },
     );
-  }
 
-  Widget UnusableListTile(CustomFunction customFunction) {
-    return ListTile(
-        key: ValueKey(customFunction.funcName),
-        title: Text(
-          customFunction.funcName,
-          style: TextStyle(color: Colors.white),
-        ),
-        trailing: IconButton(
-            icon: Icon(
-              Icons.add_circle_outline,
-              color: Colors.blue,
-            ),
-            onPressed: () {
-              var selectedTiles = unusableListTiles
-                  .firstWhere((x) => x.funcName == customFunction.funcName);
-              uListViewState.AddFunc(selectedTiles);
-              mListViewState.mAddFunc(selectedTiles);
-              RemoveFunc(selectedTiles);
-            }));
-  }
-}
+  
 
-//usableListView
-class UsableListView extends StatefulWidget {
-  @override
-  _UsableListViewState createState() =>
-      uListViewState = new _UsableListViewState();
-}
-
-class _UsableListViewState extends State<UsableListView> {
-  List<CustomFunction> usableListTiles = [];
-
-  ScrollController _scrollController;
-  double offset = 0.0;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController = ScrollController() //keepScrollOffset: false removed
-      ..addListener(() {
-        setState(() {
-          offset = _scrollController.offset;
-          // force a refresh so the app bar can be updated
-        });
-      });
-  }
-
-  void AddFunc(CustomFunction funcs) {
-    setState(() {
-      usableListTiles.add(funcs);
-    });
-  }
-
-  void RemoveFunc(CustomFunction funcs) {
-    setState(() {
-      usableListTiles.remove(funcs);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //return ROListView();
-    return Theme(
-      data: ThemeData(
-        canvasColor: Color(0x59474646),
-      ),
-      child: ReorderableListView(
-          onReorder: (int oldindex, int newindex) {
-            setState(() {
-              if (oldindex < newindex) {
-                // removing the item at oldIndex will shorten the list by 1.
-                newindex -= 1;
-              }
-              print(oldindex);
-              print(newindex);
-              var tempfunc = usableListTiles[oldindex];
-              print(usableListTiles[oldindex]);
-              usableListTiles.removeAt(oldindex);
-              usableListTiles.insert(newindex, tempfunc);
-            });
-          },
-          scrollController: uListViewState._scrollController,
-          scrollDirection: Axis.vertical,
-          children: List.generate(
-            usableListTiles.length,
-                (index) {
-              return UsableListTile(usableListTiles[index]);
-            },
-          )),
-    );
-
-    // return ListView.builder(
-    //   itemCount: usableListTiles.length,
-    //   itemBuilder: (context, index) {
-    //     return UsableListTile(usableListTiles[index]);
+    // return ReorderableListView(
+    //   onReorder: (int oldindex, int newindex) {
+    //     setState(() {
+    //       var tempfuncs = FunctionManager._instance.usingFunctions[oldindex];
+    //       FunctionManager._instance.usingFunctions.add(tempfuncs);
+    //     });
     //   },
+    //   children: List.generate(Listss.length, (index){return emptyCard(Listss[index]);}),
     // );
   }
 
-  Widget UsableListTile(CustomFunction customFunction) {
-    return ListTile(
-        key: ValueKey(customFunction.funcName),
-        title: Text(
-          customFunction.funcName,
-          style: TextStyle(color: Colors.white),
-        ),
-        trailing: IconButton(
-            icon: Icon(
-              Icons.remove_circle_outline,
-              color: Colors.red,
-            ),
-            onPressed: () {
-              var selectedTiles = usableListTiles
-                  .firstWhere((x) => x.funcName == customFunction.funcName);
-              unListViewState.AddFunc(selectedTiles);
-              RemoveFunc(selectedTiles);
-            }));
+  void mSetFunc(List<CustomFunction> funcs) {
+    setState(() {
+      _fm.usingFunctions = funcs;
+    });
   }
 }
+
+class FunctionManager{
+  static final FunctionManager _instance = FunctionManager._internal();
+  // static FunctionManager get instance =>
+  //   (_instance != null) ? _instance : _instance = 
+  List<CustomFunction> usingFunctions;
+
+  factory FunctionManager() {
+    return _instance;
+  }
+
+  FunctionManager._internal() { //클래스가 최초 생성될때 1회 발생
+    //초기화 코드
+    //usingFunctions = getfromdevice
+    usingFunctions = [
+      CustomFunction("MaskMap"),
+      CustomFunction("ReliefHospitals"),
+      CustomFunction("SidoInfos"),
+    ]; 
+  }
+}
+
---
+++ costum_app/lib/setting.dart
@@ -0,0 +1,251 @@
+import 'package:costumapp/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final _fm = FunctionManager();
+
+class Performance extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Setting"),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            // image: DecorationImage(
+            //     image: AssetImage("lib/img/background.jpg"), fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: 400,
+                height: 270,
+                padding: const EdgeInsets.all(6),
+                child: Card(
+                  color: Color(0x59474646),
+                  child: UsableListView(),
+                ),
+              ),
+              Container(
+                width: 400,
+                height: 270,
+                padding: const EdgeInsets.all(6),
+                child: Card(
+                  color: Color(0x59474646),
+                  child: UnusableListView(),
+                ),
+              ),
+              Container(
+                width: 70,
+                height: 30,
+                child: RaisedButton(
+                  padding: const EdgeInsets.all(5),
+                  onPressed: () {
+                    mListViewState.mSetFunc(uListViewState.usableListTiles);
+                    //_fm.usingFunctions = uListViewState.usableListTiles;
+                    Navigator.pop(context);
+                  },
+                  child: Text('Go'),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+//unsuableList와 usableList가 필요 unsuable에서 usable로 해당 데이터가 삽입되어야함
+// 삽입될때 인덱스는 어떻게? for문으로 위치를 찾아줘야하나?
+// 삭제또한 마찬가지
+// usable은 reorderable이어야함
+// 이 모든게 성공하면 globalkey로 앞 화면에 적용하고
+// 앞화면의 데이터를 주거나 연결해줘야할 듯
+class CustomFunction {
+  String funcName;
+  //other elements
+
+  CustomFunction(String _funcName) {
+    funcName = _funcName;
+    //other elements initialize
+  }
+}
+
+
+
+
+_UnusableListViewState unListViewState;
+_UsableListViewState uListViewState;
+
+String unusableItem = "집갈래";
+
+//listview builder로 생성 list.length만큼
+class UnusableListView extends StatefulWidget {
+  @override
+  _UnusableListViewState createState() =>
+      unListViewState = new _UnusableListViewState();
+}
+
+class _UnusableListViewState extends State<UnusableListView> {
+  List<CustomFunction> unusableListTiles;
+
+  // @override
+  // bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    // TODO: Need fix
+    super.initState();
+    unusableListTiles  = [
+      CustomFunction("test 1"),
+      CustomFunction("test 2"),
+      CustomFunction("test 3"),
+    ]; 
+  }
+
+  void AddFunc(CustomFunction funcs) {
+    setState(() {
+      unusableListTiles.add(funcs);
+    });
+  }
+
+  void RemoveFunc(CustomFunction funcs) {
+    setState(() {
+      unusableListTiles.remove(funcs);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: unusableListTiles.length,
+      itemBuilder: (context, index) {
+        return UnusableListTile(unusableListTiles[index]);
+      },
+    );
+  }
+
+  Widget UnusableListTile(CustomFunction customFunction) {
+    return ListTile(
+        key: ValueKey(customFunction.funcName),
+        title: Text(
+          customFunction.funcName,
+          style: TextStyle(color: Colors.white),
+        ),
+        trailing: IconButton(
+            icon: Icon(
+              Icons.add_circle_outline,
+              color: Colors.blue,
+            ),
+            onPressed: () {
+              var selectedTiles = unusableListTiles
+                  .firstWhere((x) => x.funcName == customFunction.funcName);
+              uListViewState.AddFunc(selectedTiles);
+              //mListViewState.mAddFunc(selectedTiles); //TODO:Fix
+              RemoveFunc(selectedTiles);
+            }));
+  }
+}
+
+//usableListView
+class UsableListView extends StatefulWidget {
+  @override
+  _UsableListViewState createState() =>
+      uListViewState = new _UsableListViewState();
+}
+
+class _UsableListViewState extends State<UsableListView>{
+  List<CustomFunction> usableListTiles = [];
+
+  ScrollController _scrollController;
+  double offset = 0.0;
+
+  // @override
+  // bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+    usableListTiles = _fm.usingFunctions;
+    _scrollController = ScrollController() //keepScrollOffset: false removed
+      ..addListener(() {
+        setState(() {
+          offset = _scrollController.offset;
+          // force a refresh so the app bar can be updated
+        });
+      });
+  }
+
+  void AddFunc(CustomFunction funcs) {
+    setState(() {
+      usableListTiles.add(funcs);
+    });
+  }
+
+  void RemoveFunc(CustomFunction funcs) {
+    setState(() {
+      usableListTiles.remove(funcs);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //return ROListView();
+    return Theme(
+      data: ThemeData(
+        canvasColor: Color(0x59474646),
+      ),
+      child: ReorderableListView(
+          onReorder: (int oldindex, int newindex) {
+            setState(() {
+              if (oldindex < newindex) {
+                // removing the item at oldIndex will shorten the list by 1.
+                newindex -= 1;
+              }
+              print(oldindex);
+              print(newindex);
+              var tempfunc = usableListTiles[oldindex];
+              print(usableListTiles[oldindex]);
+              usableListTiles.removeAt(oldindex);
+              usableListTiles.insert(newindex, tempfunc);
+            });
+          },
+          scrollController: uListViewState._scrollController,
+          scrollDirection: Axis.vertical,
+          children: List.generate(
+            usableListTiles.length,
+                (index) {
+              return UsableListTile(usableListTiles[index]);
+            },
+          )),
+    );
+
+
+    // return ListView.builder(
+    //   itemCount: usableListTiles.length,
+    //   itemBuilder: (context, index) {
+    //     return UsableListTile(usableListTiles[index]);
+    //   },
+    // );
+  }
+
+  Widget UsableListTile(CustomFunction customFunction) {
+    return ListTile(
+        key: ValueKey(customFunction.funcName),
+        title: Text(
+          customFunction.funcName,
+          style: TextStyle(color: Colors.white),
+        ),
+        trailing: IconButton(
+            icon: Icon(
+              Icons.remove_circle_outline,
+              color: Colors.red,
+            ),
+            onPressed: () {
+              var selectedTiles = usableListTiles
+                  .firstWhere((x) => x.funcName == customFunction.funcName);
+              unListViewState.AddFunc(selectedTiles);
+              RemoveFunc(selectedTiles);
+            }));
+  }
+}
